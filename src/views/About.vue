84520301fbdec5fd1e17cb30e7c2000f9c127ef9
Ernie Jeash C. Villahermosa <irenesejah29@gmail.com>
2020-01-30 17:40:36 +0800

    added floating effect

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,56 +11,64 @@
 <script>
 import { gsap } from "gsap";
 export default {
-  mounted() {
-    console.clear();
+  data: () => ({
+    randomX: 0,
+    randomY: 0,
+    randomTime: 0,
+    randomTime2: 0,
+    randomAngle: 0
+  }),
 
+  mounted() {
     const can = document.querySelector(".can > img");
 
-    const randomX = random(10, 20);
-    const randomY = random(20, 30);
-    const randomTime = random(3, 5);
-    const randomTime2 = random(5, 10);
-    const randomAngle = random(8, 12);
+    this.randomX = this.random(10, 20);
+    this.randomY = this.random(20, 30);
+    this.randomTime = this.random(3, 5);
+    this.randomTime2 = this.random(5, 10);
+    this.randomAngle = this.random(8, 12);
 
     gsap.set(can, {
-      x: randomX(-1),
-      y: randomX(1),
-      rotation: randomAngle(-1)
+      x: this.randomX(-1),
+      y: this.randomX(1),
+      rotation: this.randomAngle(-1)
     });
 
-    moveX(can, 1);
-    moveY(can, -1);
-    rotate(can, 1);
+    this.moveX(can, 1);
+    this.moveY(can, -1);
+    this.rotate(can, 1);
+  },
 
-    function rotate(target, direction) {
-      gsap.to(target, randomTime2(), {
-        rotation: randomAngle(direction),
+  methods: {
+    rotate(target, direction) {
+      gsap.to(target, this.randomTime2(), {
+        rotation: this.randomAngle(direction),
         // delay: randomDelay(),
         ease: "Sine.easeInOut",
-        onComplete: rotate,
+        onComplete: this.rotate,
         onCompleteParams: [target, direction * -1]
       });
-    }
+    },
 
-    function moveX(target, direction) {
-      gsap.to(target, randomTime(), {
-        x: randomX(direction),
+    moveX(target, direction) {
+      gsap.to(target, this.randomTime(), {
+        x: this.randomX(direction),
         ease: "Sine.easeInOut",
-        onComplete: moveX,
+        onComplete: this.moveX,
         onCompleteParams: [target, direction * -1]
       });
-    }
+    },
 
-    function moveY(target, direction) {
-      gsap.to(target, randomTime(), {
-        y: randomY(direction),
+    moveY(target, direction) {
+      gsap.to(target, this.randomTime(), {
+        y: this.randomY(direction),
         ease: "Sine.easeInOut",
-        onComplete: moveY,
+        onComplete: this.moveY,
         onCompleteParams: [target, direction * -1]
       });
-    }
+    },
 
-    function random(min, max) {
+    random(min, max) {
       const delta = max - min;
       return (direction = 1) => (min + delta * Math.random()) * direction;
     }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="game">
-    <div class="hook" id="hook">CLICK TO CAST</div>
-
+    <div class="indicator" id="indicator">CLICK TO CAST</div>
     <div class="game-content">
       <div class="pond" @mouseover="mouseover" @mouseout="mouseout"></div>
     </div>
@@ -12,30 +11,87 @@
 import { gsap } from "gsap";
 export default {
   data: () => ({
-    hook: null
+    indicator: null,
+
+    randomX: 0,
+    randomY: 0,
+    randomTime: 0,
+    randomTime2: 0,
+    randomAngle: 0
   }),
 
   mounted() {
-    this.hook = document.getElementById("hook");
+    this.indicator = document.getElementById("indicator");
     window.addEventListener("mousemove", this.mousemove);
   },
 
   methods: {
+    floatBait() {
+      this.randomX = this.random(10, 20);
+      this.randomY = this.random(20, 30);
+      this.randomTime = this.random(3, 5);
+      this.randomTime2 = this.random(5, 10);
+      this.randomAngle = this.random(8, 12);
+
+      gsap.set(this.indicator, {
+        x: this.randomX(-1),
+        y: this.randomX(1),
+        rotation: this.randomAngle(-1)
+      });
+
+      this.moveX(this.indicator, 1);
+      this.moveY(this.indicator, -1);
+      this.rotate(this.indicator, 1);
+    },
+
     mouseover() {
-      gsap.to(this.hook, 0.15, { scale: 1, autoAlpha: 1 });
+      gsap.to(this.indicator, 0.15, { scale: 1, autoAlpha: 1 });
     },
 
     mousemove(e) {
-      gsap.to(this.hook, 0.2, {
+      gsap.to(this.indicator, 0.2, {
         css: {
-          left: e.pageX + this.hook.clientWidth / 2,
-          top: e.pageY + this.hook.clientHeight / 2
+          left: e.pageX + this.indicator.clientWidth / 2,
+          top: e.pageY + this.indicator.clientHeight / 2
         }
       });
     },
 
     mouseout() {
-      gsap.to(this.hook, 0.15, { scale: 0, autoAlpha: 1 });
+      gsap.to(this.indicator, 0.1, { scale: 0, autoAlpha: 1 });
+    },
+
+    rotate(target, direction) {
+      gsap.to(target, this.randomTime2(), {
+        rotation: this.randomAngle(direction),
+        // delay: randomDelay(),
+        ease: "Sine.easeInOut",
+        onComplete: this.rotate,
+        onCompleteParams: [target, direction * -1]
+      });
+    },
+
+    moveX(target, direction) {
+      gsap.to(target, this.randomTime(), {
+        x: this.randomX(direction),
+        ease: "Sine.easeInOut",
+        onComplete: this.moveX,
+        onCompleteParams: [target, direction * -1]
+      });
+    },
+
+    moveY(target, direction) {
+      gsap.to(target, this.randomTime(), {
+        y: this.randomY(direction),
+        ease: "Sine.easeInOut",
+        onComplete: this.moveY,
+        onCompleteParams: [target, direction * -1]
+      });
+    },
+
+    random(min, max) {
+      const delta = max - min;
+      return (direction = 1) => (min + delta * Math.random()) * direction;
     }
   }
 };
@@ -47,7 +103,7 @@ export default {
 $hookWidth: 100px;
 $hookHeight: 50px;
 
-.hook {
+.indicator {
   position: absolute;
   width: $hookWidth;
   height: $hookHeight;
